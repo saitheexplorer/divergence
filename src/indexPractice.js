22e5fd4bfe561ed5800eccceee4850f6071dd0db
Rajan Makanji <rajanmakanji@gmail.com>
2016-07-28 23:08:50 -0500

    change function call from checkMatch to checkAnswer

--- src/indexPractice.js
+++ src/indexPractice.js
@@ -1,2 +1,12 @@
 // clicking submit in html page
 // triggers this fxn (entire script)
+
+document.getElementById("userWord")
+    .addEventListener("keydown", function(event) {
+
+    if (event.keyCode == 13){
+      var userWord = document.getElementById("userWord").value;
+      console.log("User has submitted Answer");
+      checkAnswer(userWord, states);
+    }
+});
