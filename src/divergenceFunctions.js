65ebf826da61768c4c3dd6ff578cb70d87c53e28
Rajan Makanji <rajanmakanji@gmail.com>
2016-07-28 21:11:23 -0500

    display error messages if checkMatch is called incorrectly
    add error messages to console if missing arguments

--- src/divergenceFunctions.js
+++ src/divergenceFunctions.js
@@ -2,15 +2,22 @@
 
 function checkMatch(userWord, wordBank){
   userWord = userWord.toLowerCase();
-  if(wordBank.indexOf(userWord) > -1) {
-    document.getElementById('form2').innerHTML ="Nice Job! " + userWord + " is in the word bank";
-    console.log("Answer is valid");
-    return 1;
+  // if arguments are entered correctly
+  if (userWord && wordBank){
+    if(wordBank.indexOf(userWord) > -1) {
+      document.getElementById('form2').innerHTML ="Nice Job! " + userWord + " is in the word bank";
+      console.log("Answer is valid");
+      return 1;
+    }
+    else {
+      document.getElementById('form2').innerHTML ="Invalid Answer: " + userWord + " is NOT in the word bank";
+      console.log("Answer is invalid");
+      return 0;
+    }
   }
-  else {
-    document.getElementById('form2').innerHTML ="Invalid Answer: " + userWord + " is NOT in the word bank";
-    console.log("Answer is invalid");
-    return 0;
+  //display error messages
+  else{
+    console.log("The function checkMatch is missing one or more arguments");
   }
 }
 
